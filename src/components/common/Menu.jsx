d67245d4c06916f8de7a39f3bbb897ccfa3d1066
Prado Nicolas <nicolas88prado@gmail.com>
2024-08-12 21:26:03 -0300

    agregue el maquetado necesario para el Menu

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../../assets/logo.png";
 
 const Menu = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Navbar expand="lg" bg="dark" variant="dark">
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <img src={logo} alt="Logo" className="imgLogo" width="160px" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarNav" />
+        <Navbar.Collapse id="navbarNav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#" className="text-white tamanioOpcionesMenu">
+              Inicio
+            </Nav.Link>
+            <Nav.Link href="#" className="text-white tamanioOpcionesMenu">
+              Administrador
+            </Nav.Link>
+            <Nav.Link href="#" className="text-white tamanioOpcionesMenu">
+              Contacto
+            </Nav.Link>
+            <Nav.Link href="#" className="text-white tamanioOpcionesMenu">
+              Nosotros
+            </Nav.Link>
+            <Nav.Link href="#" className="text-white tamanioOpcionesMenu">
+              <i className="bi bi-cart"></i> Compras
+            </Nav.Link>
+            <Nav.Link href="#" className="text-white tamanioOpcionesMenu">
+              <i className="bi bi-person"></i> Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Menu;
